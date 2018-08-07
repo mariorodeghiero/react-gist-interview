79b8edb13e555153419580c5193ca1cc6577d42d
mariorodeghiero <mario.rodeghiero@gmail.com>
2018-08-07 11:25:18 -0300

    add seconds timer

--- src/Components/timer/Timer.js
+++ src/Components/timer/Timer.js
@@ -2,13 +2,27 @@ import React, { Component } from "react";
 import "./Timer.scss";
 
 class Timer extends Component {
-  state = {};
+  constructor(props) {
+    super(props);
+    this.state = { seconds: 0 };
+  }
+
+  tick() {
+    this.setState(prevState => ({
+      seconds: prevState.seconds + 1
+    }));
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
   render() {
-    return (
-      <div className="timer">
-        <p>09:30</p>
-      </div>
-    );
+    return <div className="timer">Seconds : {this.state.seconds}</div>;
   }
 }
 
