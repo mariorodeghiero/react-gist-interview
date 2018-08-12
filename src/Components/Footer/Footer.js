cc3222c1827b4efdf0f03f8cb2342f53c78ee4ad
mariorodeghiero <mario.rodeghiero@gmail.com>
2018-08-12 10:52:25 -0300

    edited layout scoreboard and cards

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
-
+import "./Footer.scss";
 class Footer extends Component {
   state = {};
   render() {
     return (
       <div>
         <footer className="bg-white text-muted text-center p-4">
-          @By Mário Rodeghiero
+          ⚖️ MIT License
         </footer>
       </div>
     );
--- src/Components/score/Score.js
+++ src/Components/score/Score.js
@@ -1,18 +1,32 @@
 import React, { Component } from "react";
+import "./Score.scss";
 
 class Score extends Component {
   state = {};
   render() {
     return (
-      <div className="card w-50 mx-auto">
-        <div className="card-header text-center">
-          <h2 className="text-muted">SCOREBOARD</h2>
+      <div className="container">
+        <h2 className="title-score text-center text-muted"> Dashboard </h2>
+        <div className="row">
+          <div className="card w-25 shadow mx-auto">
+            <div className="card-header text-center"> Questions</div>
+            <p className="text-muted pl-4 pt-4">
+              Number of questions : {this.props.number}
+            </p>
+          </div>
+          <div className="card w-25 shadow mx-auto">
+            <div className="card-header text-center"> Wrong answers </div>
+            <p className="text-muted pl-4 pt-4">
+              Wrong answers : {this.props.number}
+            </p>
+          </div>
+          <div className="card w-25 shadow mx-auto">
+            <div className="card-header text-center"> Right answers </div>
+            <p className="text-muted pl-4">
+              Total points : {this.props.points}
+            </p>
+          </div>
         </div>
-
-        <p className="text-muted pl-4 pt-4">
-          Number of questions : {this.props.number}
-        </p>
-        <p className="text-muted pl-4">Total points : {this.props.points}</p>
       </div>
     );
   }
