a8ca257f075bedc1f89e4b1a0a77e9640fd61dfc
mariorodeghiero <mario.rodeghiero@gmail.com>
2018-08-12 21:08:33 -0300

    edited layout scoreboard and cards

--- README.md
+++ README.md
@@ -14,3 +14,9 @@ This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md
 
 [devpleno]: https://www.devpleno.com
 [firebase]: https://firebase.google.com
+
+<!-- <div>Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div> -->
+
+<!--
+Icon made by [author link] from www.flaticon.com
+Icon made by Freepik from www.flaticon.com  -->
--- src/Components/score/Score.js
+++ src/Components/score/Score.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import "./Score.scss";
 
+import Warning from "../../warning.svg";
+
 class Score extends Component {
   state = {};
   render() {
@@ -9,22 +11,37 @@ class Score extends Component {
         <h2 className="title-score text-center text-muted"> Dashboard </h2>
         <div className="row">
           <div className="card w-25 shadow mx-auto">
-            <div className="card-header text-center"> Questions</div>
-            <p className="text-muted pl-4 pt-4">
-              Number of questions : {this.props.number}
-            </p>
+            <div className="row">
+              <div>
+                <img className="icon" src={Warning} alt="Warging" />
+              </div>
+              <p className="result">{this.props.number}</p>
+            </div>
+            <div className="text-muted text-center">
+              <p>Questions</p>
+            </div>
           </div>
           <div className="card w-25 shadow mx-auto">
-            <div className="card-header text-center"> Wrong answers </div>
-            <p className="text-muted pl-4 pt-4">
-              Wrong answers : {this.props.number}
-            </p>
+            <div className="row">
+              <div>
+                <img className="icon" src={Warning} alt="Warging" />
+              </div>
+              <p className="result">{this.props.number}</p>
+            </div>
+            <div className="text-muted text-center">
+              <p>Wrong</p>
+            </div>
           </div>
           <div className="card w-25 shadow mx-auto">
-            <div className="card-header text-center"> Right answers </div>
-            <p className="text-muted pl-4">
-              Total points : {this.props.points}
-            </p>
+            <div className="row">
+              <div>
+                <img className="icon" src={Warning} alt="Warging" />
+              </div>
+              <p className="result">{this.props.points}</p>
+            </div>
+            <div className="text-muted text-center">
+              <p>Sucessful</p>
+            </div>
           </div>
         </div>
       </div>
